7915daaf18a1ba6c49899bb3b06e49c31e22ecc9
Gabriel <Goldschmidt>
2021-04-01 15:47:24 -0300

    Create repository
    First repository

---
+++ R/add_squares.R
@@ -0,0 +1,27 @@
+#' Title
+#'
+#' Description
+#'
+#' Details
+#'
+#' @author Kurt Wirth
+#'
+#' @param x A number.
+#'
+#' @param y A number.
+#'
+#' @return The sum of \code{x}^2 and \code{y}^2
+#'
+#' @examples
+#'
+#' add_squares(1, 2)
+#'
+#' add_squares(1, -2)
+#'
+#' @export
+
+add_squares = function(num1, num2){
+
+  my_sq(num1) + my_sq(num2)
+
+}
